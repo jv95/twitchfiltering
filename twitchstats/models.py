d5a1f71f7491f2b758df7a07b707c2e6b540e4df
jv95 <10341166+jv95@users.noreply.github.com>
2019-11-16 21:07:05 +0100

    app created, init homepage..

--- twitchstats/models.py
+++ twitchstats/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class live_streams(models.Model):
+    id = models.IntegerField().primary_key=True
+    user_id = models.IntegerField()
+    user_name = models.TextField()
+    game_id = models.IntegerField()
+    type = models.TextField()
+    title = models.TextField()
+    viewer_count = models.IntegerField()
+    started_at = models.DateTimeField()
+    language = models.TextField()
+    thumbnail_url = models.TextField()
+    tag_ids = models.TextField()
