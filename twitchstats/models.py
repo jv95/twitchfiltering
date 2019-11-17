66e30dcbf9cae2e4335686e08b42bd4c677af62e
jv95 <10341166+jv95@users.noreply.github.com>
2019-11-17 01:34:34 +0100

    new model for performance info

--- twitchstats/models.py
+++ twitchstats/models.py
@@ -2,14 +2,22 @@ from django.db import models
 
 
 class live_streams(models.Model):
-    id = models.IntegerField().primary_key=True
-    user_id = models.IntegerField()
-    user_name = models.TextField()
-    game_id = models.IntegerField()
-    type = models.TextField()
-    title = models.TextField()
-    viewer_count = models.IntegerField()
-    started_at = models.DateTimeField()
-    language = models.TextField()
-    thumbnail_url = models.TextField()
-    tag_ids = models.TextField()
+    stream_id = models.TextField(default=None, blank=True, null=True,max_length=1024)
+    user_id = models.TextField(default=None, blank=True, null=True)
+    user_name = models.TextField(default=None, blank=True, null=True)
+    game_id = models.TextField(default=None, blank=True, null=True)
+    type = models.TextField(default=None, blank=True, null=True)
+    title = models.TextField(default=None, blank=True, null=True)
+    viewer_count = models.BigIntegerField(default=None, blank=True, null=True)
+    started_at = models.DateTimeField(default=None, blank=True, null=True)
+    language = models.TextField(default=None, blank=True, null=True)
+    thumbnail_url = models.TextField(default=None, blank=True, null=True)
+    tag_ids = models.TextField(default=None, blank=True, null=True)
+
+class performance_info(models.Model):
+    date = models.TextField(default=None, blank=True, null=True)
+    number_of_streams = models.TextField(default=None, blank=True, null=True)
+    data_requesting = models.TextField(default=None, blank=True, null=True)
+    data_uploading = models.TextField(default=None, blank=True, null=True)
+    final_time = models.TextField(default=None, blank=True, null=True)
+    request_count = models.TextField(default=None, blank=True, null=True)
