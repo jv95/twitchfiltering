65f67e50a572b039ff77d54bbdca524554f81f5d
jv95 <10341166+jv95@users.noreply.github.com>
2019-11-30 19:21:29 +0100

    refactor

--- twitchstats/django_script.py
+++ twitchstats/django_script.py
@@ -3,11 +3,11 @@
 
 # create new db everytime - delete the old one and replace it with the new one?
 
-from games_manager import GamesManager
-from streams_manager import StreamsManager
+from . import games_manager
+from . import streams_manager
 
-get_all_streams = StreamsManager()
-get_all_games = GamesManager()
+get_all_streams = streams_manager.StreamsManager()
+get_all_games = games_manager.GamesManager()
 
 get_all_streams.get_streams()
 get_all_games.get_games()
