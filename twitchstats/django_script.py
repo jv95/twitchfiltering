b72f7d69425cf3a205215ecfb822900492085cdf
jv95 <10341166+jv95@users.noreply.github.com>
2019-12-07 16:23:04 +0100

    logger implemented

--- twitchstats/django_script.py
+++ twitchstats/django_script.py
@@ -2,11 +2,16 @@
 # coding=utf8
 
 
+import logging
+
 from modules import games_manager
 from modules import streams_manager
 
-get_all_streams = streams_manager.StreamsManager()
-get_all_games = games_manager.GamesManager()
-
-get_all_streams.get_streams()
-get_all_games.get_games()
+logging.basicConfig(filename='twitch_stats.log', format='%(asctime)s')
+try:
+    get_all_streams = streams_manager.StreamsManager()
+    get_all_games = games_manager.GamesManager()
+    get_all_streams.get_streams()
+    get_all_games.get_games()
+except Exception as e:
+    logging.exception('Exception has occured!')
