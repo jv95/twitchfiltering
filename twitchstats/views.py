dc9d1ae6773aed5d449a425466a7595d3f710221
jv95 <10341166+jv95@users.noreply.github.com>
2019-12-08 15:32:46 +0100

    refactor

--- twitchstats/views.py
+++ twitchstats/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
-
+import sys
+import os
+import django
+sys.path.append('/home/bjork/www/bjorktest.com/web')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web.settings")
+django.setup()
 from twitchstats.models import active_table_games, active_table_streams
 
+
 def index(request):
-    cat_streams = active_table_streams.objects.values()
-    cat_games = active_table_games.objects.values()
-    return render(request, 'templates/base.html',
-                  {'cat_streams': cat_streams[1]['active_table'], 'cat_games': cat_games[1]['active_table']})
+    cat_streams = current_active_table = active_table_streams.objects.values().order_by('-id').first()
+    cat_games = current_active_table = active_table_games.objects.values().order_by('-id').first()
+
+    return render(request, 'templates/base.html', {'cat_streams': cat_streams, 'cat_games': cat_games})
