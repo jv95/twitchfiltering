eb3c9781811f6fa8f190222d88d9a9cb76009b4d
jv95 <10341166+jv95@users.noreply.github.com>
2019-12-08 15:39:49 +0100

    refactor

--- twitchstats/views.py
+++ twitchstats/views.py
@@ -1,15 +1,19 @@
-from django.shortcuts import render
-import sys
 import os
+import sys
+
 import django
-sys.path.append('/home/bjork/www/bjorktest.com/web')
+import yaml
+from django.shortcuts import render
+
+with open('settings.yaml', 'r') as yamlfile: cfg = yaml.load(yamlfile)
+sys.path.append(cfg['environment']['sys_path_append'])
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web.settings")
 django.setup()
 from twitchstats.models import active_table_games, active_table_streams
 
 
 def index(request):
-    cat_streams = current_active_table = active_table_streams.objects.values().order_by('-id').first()
-    cat_games = current_active_table = active_table_games.objects.values().order_by('-id').first()
+    cat_streams = active_table_streams.objects.values().order_by('-id').first()
+    cat_games = active_table_games.objects.values().order_by('-id').first()
 
     return render(request, 'templates/base.html', {'cat_streams': cat_streams, 'cat_games': cat_games})
