05fcd4c8b18c2e34c83c1b7647fa3905d8e8de73
jv95 <10341166+jv95@users.noreply.github.com>
2019-12-25 23:55:38 +0100

    added form for streams filtering, form is filled by games from db

---
+++ twitchstats/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+import os
+import sys
+
+import django
+import yaml
+from web.settings import BASE_DIR
+
+with open(BASE_DIR + '/twitchstats/settings.yaml', 'r') as yamlfile: cfg = yaml.load(yamlfile)
+sys.path.append(cfg['environment']['sys_path_append'])
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web.settings")
+django.setup()
+from twitchstats.models import game_identity, active_table_games, game_identity2
+
+active_table_streams = active_table_games.objects.values()
+game_list = game_identity2.objects.values() if 'game_identity2' in active_table_streams else game_identity.objects.values()
+game_list_filtered = [d['game_name'] for d in game_list]
+lenght = list(range(0, len(game_list_filtered)))
+game_choices = zip(lenght, game_list_filtered)
+
+
+class GetStreamsForm(forms.Form):
+    game = forms.ChoiceField(choices=game_choices, required=True,
+                             widget=forms.Select(attrs={'class': 'form-control'}))
+    max_followers = forms.IntegerField(min_value=0, required=False,
+                                       widget=forms.NumberInput(attrs={'class': 'form-control'}))
+    max_viewers = forms.IntegerField(min_value=0, required=False,
+                                     widget=forms.NumberInput(attrs={'class': 'form-control'}))
--- twitchstats/views.py
+++ twitchstats/views.py
@@ -4,18 +4,17 @@ import sys
 import django
 import yaml
 from django.shortcuts import render
-
 from web.settings import BASE_DIR
 
 with open(BASE_DIR + '/twitchstats/settings.yaml', 'r') as yamlfile: cfg = yaml.load(yamlfile)
 sys.path.append(cfg['environment']['sys_path_append'])
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web.settings")
 django.setup()
-from twitchstats.models import active_table_games, active_table_streams
-
+from twitchstats.forms import GetStreamsForm
 
 def index(request):
-    cat_streams = active_table_streams.objects.values().order_by('-id').first()
-    cat_games = active_table_games.objects.values().order_by('-id').first()
+    form = GetStreamsForm()
+    if form.is_valid():
+        form.clean()
 
-    return render(request, 'templates/base.html', {'cat_streams': cat_streams, 'cat_games': cat_games})
+    return render(request, 'templates/base.html', {'form': form})
