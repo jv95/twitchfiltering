6fa4584a6824ecfcf4a9b85cc667f0229b942499
jv95 <10341166+jv95@users.noreply.github.com>
2020-01-18 12:15:30 +0100

    refactor + bootstrap

--- jv95/views.py
+++ jv95/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 
+
 def index(request):
     if request.method == 'POST':
         name = request.POST.get('name')
         email = request.POST.get('email')
         message = request.POST.get('message')
-
         with open('messages_from_contact.txt', 'a') as myfile:
-            myfile.write(name + ' ' + email + ' ' + message)
+            myfile.write('\n' + name + ' ' + email + ' ' + message)
 
     return render(request, 'jv95/templates/index.html')
