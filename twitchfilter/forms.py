eec1a4f8cb9d9ddbd8248da1e2924b5ba29d4b8f
jv95 <10341166+jv95@users.noreply.github.com>
2020-05-10 18:32:09 +0200

    forms updated with fix

--- twitchfilter/forms.py
+++ twitchfilter/forms.py
@@ -15,16 +15,16 @@ from twitchfilter.models import game_identity, live_streams
 
 game_list = game_identity.objects.values().order_by('game_name')
 game_list_filtered = [d['game_name'] for d in game_list]
-game_list_filtered[0]=''
+if game_list_filtered: game_list_filtered[0]=''
 
 id_list_filtered = [d['game_id'] for d in game_list]
-id_list_filtered[0]=''
+if id_list_filtered: id_list_filtered[0]=''
 
 game_choices = zip(id_list_filtered, game_list_filtered)
 
 streams_list = live_streams.objects.values('language').order_by('language').distinct()
 language_list = [d['language'] for d in streams_list]
-language_list[0]=''
+if language_list: language_list[0]=''
 language_to_choice = zip(language_list, language_list)
 
 
